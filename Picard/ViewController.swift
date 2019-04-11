f988fd75199773c9d7246fbbef325937250942bf
Le Ba Cong <conglb@yahoo.com>
2019-04-11 08:30:04 +0700

    First commit git add .

--- Picard/ViewController.swift
+++ Picard/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    // MARK:
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks.
+        usernameTextField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        loginLabel.text = "Hello " + usernameTextField.text + '!'
     }
 
 
+    @IBAction func loginButton(_ sender: Any) {
+        usernameTextField.text = "again"
+        passwordTextField.text = "again"
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Picard' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Picard
+
+  target 'PicardTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PicardUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
